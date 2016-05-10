41e5a2eb55a08b0c9447faf4e5d157a05f4eba14
Parker <kallebloomckwist@gmail.com>
2016-05-10 13:56:27 +0000

    Changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,9 @@
 $(document).ready(function($){
     $('#lightslider').lightSlider({
         
-        item:4,
+        item:1,
         loop:true,
         speed:2000,
-        slideMargin:20,
-        responsive:[
-            {
-             breakpoint:800,
-             settings:{
-                 item:2,
-                 slideMargin:10,
-             }
-            },
-            {
-            breakpoint:400,
-             settings:{
-                 item:1,
-                slideMargin:10,
-             }
-            }
-            ],
-            
             autoWidth:true,
             adaptiveHeight:true,
             
